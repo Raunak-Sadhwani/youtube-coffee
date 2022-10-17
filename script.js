6fbb3118b371c56b4cc1150fca86a834c2360bb2
Raunak <raunak200308@gmail.com>
2022-10-18 00:14:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function onMenuClick() {
+    var navbar = document.getElementById('nav-bar');
+    var mobile = 'mobile'
+    navbar.classList.toggle(mobile)
+    var slide = 'slide-in'
+    navbar.classList.toggle(slide)
+}
